43f569410b8770f9371d692f2bc10a753f9e6885
Sebastian Hütter <sebastian.huetter@ovgu.de>
2017-08-02 01:41:18 +0200

    New setup script (InnoSetup); Version Bump

--- Docear4Word/Docear4Word/Properties/AssemblyInfo.cs
+++ Docear4Word/Docear4Word/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("debug")]
 [assembly: AssemblyCompany("Docear")]
 [assembly: AssemblyProduct("Docear4Word")]
-[assembly: AssemblyCopyright("(c) 2012-2013 Docear")]
+[assembly: AssemblyCopyright("(c) 2012-2017 Docear")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
  
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the * as shown below:
 
-[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
  
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
